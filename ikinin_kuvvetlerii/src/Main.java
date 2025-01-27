ab6c5c9abdb6bf39c22004eaeb10f36f51b61017
hmgny <hmerveguney@hotmail.com>
2025-01-27 14:40:15 +0300

    Kullanıcıdan alınan sayıyı geçmeyecek şekilde 4 ve 5 'in kuvvetleri hesaplandı.

--- ikinin_kuvvetlerii/src/Main.java
+++ ikinin_kuvvetlerii/src/Main.java
@@ -3,15 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     int sayi;
-    int baslangic = 0;
+    int baslangic2 = 0;
+    int baslangic4 = 0;
+    int baslangic5 = 0;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Bir sayı giriniz: ");
         sayi = input.nextInt();
 
         for(int i = 1; i<=sayi; i*=2){
-            System.out.println("2'nin " + baslangic++ +"'nci kuvveti : " + i);
+            System.out.println("2'nin " + baslangic2++ +"'nci kuvveti : " + i);
         }
+        System.out.println("*******************************************************");
+        for(int i = 1; i<=sayi; i*=4){
+            System.out.println("4'ün " + baslangic4++ +"'nci kuvveti : " + i);
+        }
+        System.out.println("*******************************************************");
+        for(int i = 1; i<=sayi; i*=5){
+            System.out.println("5'in " + baslangic5++ +"'nci kuvveti : " + i);
+        }
+
 
     }
 }
